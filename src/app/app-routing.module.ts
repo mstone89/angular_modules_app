1dfc1c0cb979e28fae63527af7efc5502cfc2500
Molly Stone <molly.stone89@gmail.com>
2020-02-09 16:14:52 -0500

    Add lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,22 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 
 const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: '**', component: NotFoundComponent }
+    { 
+        path: 'collections', 
+        loadChildren: () => import('./collections/collections.module').then((m) => m.CollectionsModule)
+    },
+    { 
+        path: 'elements', 
+        loadChildren: () => import('./elements/elements.module').then((m) => m.ElementsModule)
+    },
+    { 
+        path: '', 
+        component: HomeComponent 
+    },
+    { 
+        path: '**', 
+        component: NotFoundComponent 
+    }
 ];
 
 @NgModule({

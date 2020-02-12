de0d18db0799a00d18781e7731b1cc175c0b6aec
Molly Stone <molly.stone89@gmail.com>
2020-02-11 20:21:15 -0500

    Create and implement routes for tabs component

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -6,10 +6,13 @@ import { CollectionsHomeComponent } from './collections-home/collections-home.co
 import { TableComponent } from './table/table.component';
 import { SharedModule } from '../shared/shared.module';
 import { TabsComponent } from './tabs/tabs.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 
 @NgModule({
-  declarations: [CollectionsHomeComponent, TableComponent, TabsComponent],
+  declarations: [CollectionsHomeComponent, TableComponent, TabsComponent, BiographyComponent, CompaniesComponent, PartnersComponent],
   imports: [
     CommonModule,
     CollectionsRoutingModule,

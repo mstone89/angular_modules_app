d73a70d6b461bd77ba4b387f8b82c69312e13ba9
Molly Stone <molly.stone89@gmail.com>
2020-02-11 21:01:54 -0500

    Show stats component

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ViewsRoutingModule } from './views-routing.module';
 import { ViewsHomeComponent } from './views-home/views-home.component';
 import { StatisticsComponent } from './statistics/statistics.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [ViewsHomeComponent, StatisticsComponent],
   imports: [
     CommonModule,
+    SharedModule,
     ViewsRoutingModule
   ]
 })
